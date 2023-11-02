61b3c44b30cefb4851c8f18441ce68000454a54e
3illBaby <thrillanon@outlook.com>
2023-11-02 21:30:14 +0100

    AT-37 <Development in progress>

--- contract/contracts/Student.sol
+++ contract/contracts/Student.sol
@@ -5,10 +5,18 @@ contract Student {
     address private immutable Owner;
     string public name;
     uint public age;
+    string public faculty;
+    string public department;
 
-    constructor(address _ownerAddress, string memory _name, uint _age) {
+    constructor(
+        address _ownerAddress,
+        string memory _name,
+        uint _age,
+        string memory _department
+    ) {
         Owner = _ownerAddress;
         name = _name;
         age = _age;
+        department = _department;
     }
 }
