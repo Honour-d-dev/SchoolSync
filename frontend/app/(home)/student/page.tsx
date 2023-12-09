b195d9e64faac29d04162be8d3c459bed37412b0
Honour-d-dev <honourumunna@gmail.com>
2023-12-09 19:28:15 +0100

    feat: responsive student home layout

---
+++ frontend/app/(home)/student/page.tsx
@@ -0,0 +1,140 @@
+"use client";
+import logo from "@/public/icons/logo.png";
+import Image from "next/image";
+import fileImg from "@/public/icons/notes composition.png";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Separator } from "@/components/ui/seperator";
+import { Home } from "@/public/icons/home";
+import { Activity } from "@/public/icons/activity";
+import { Book } from "@/public/icons/book";
+import { Records } from "@/public/icons/records";
+import { AdminIcon } from "@/public/icons/admin";
+import { Profile } from "@/public/icons/profile";
+import { Notification } from "@/public/icons/direct-notification";
+import { Setting } from "@/public/icons/setting";
+import { Message } from "@/public/icons/message";
+import { useState } from "react";
+import { AlignJustify, X } from "lucide-react";
+
+export default function Student() {
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <div className="bg-white w-screen h-screen flex flex-row">
+      <button
+        className={`md:hidden ${
+          showMenu ? "hidden" : ""
+        } absolute top-4 left-4`}
+        onClick={() => setShowMenu((p) => !p)}
+      >
+        {" "}
+        <AlignJustify />
+      </button>
+      {/* side menu */}
+      <div
+        className={`rounded-r-3xl h-screen min-w-[210px] w-1/5 bg-primary-400 items-center md:flex flex-col pt-12 p-2 gap-4 relative ${
+          !showMenu ? "hidden" : "flex"
+        }`}
+      >
+        <button
+          className="md:hidden absolute top-4 right-4"
+          onClick={() => setShowMenu((p) => !p)}
+        >
+          <X />
+        </button>
+        <div className="bg-white rounded flex flex-col justify-center items-center p-1 w-fit">
+          <Image src={logo} alt="logo" className="h-14 w-14" />
+          <span className="text-xs -mt-2">SchoolSync</span>
+        </div>
+        <ToggleGroup type="single" className="flex flex-col">
+          <ToggleGroupItem
+            value="dashboard"
+            className="text-white data-[state=on]:text-accent-400 data-[state=on]:border-r data-[state=on]:border-accent-400 w-full justify-start"
+          >
+            <Home />
+            <span className="text-white font-Inconsolata ml-4"> Dashboard</span>
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            value="activity"
+            className="text-white data-[state=on]:text-accent-400 data-[state=on]:border-r data-[state=on]:border-accent-400 w-full justify-start"
+          >
+            <Activity />
+            <span className="text-white font-Inconsolata ml-4"> Activity</span>
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            value="academics"
+            className="text-white data-[state=on]:text-accent-400 data-[state=on]:border-r data-[state=on]:border-accent-400 w-full justify-start"
+          >
+            <Book />
+            <span className="text-white font-Inconsolata ml-4"> Academics</span>
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            value="student records"
+            className="text-white data-[state=on]:text-accent-400 data-[state=on]:border-r data-[state=on]:border-accent-400 w-full justify-start"
+          >
+            <Records />
+            <span className="text-white font-Inconsolata ml-4">
+              Student Records
+            </span>
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            value="admin"
+            className="text-white data-[state=on]:text-accent-400 data-[state=on]:border-r data-[state=on]:border-accent-400 w-full justify-start"
+          >
+            <AdminIcon />
+            <span className="text-white font-Inconsolata ml-4">Admin</span>
+          </ToggleGroupItem>
+
+          <Separator className="bg-accent-400 my-8" />
+
+          <ToggleGroupItem
+            value="profile"
+            className="text-white data-[state=on]:text-primary-400 data-[state=on]:bg-white font-Inconsolata gap-4 w-full justify-start"
+          >
+            <Profile />
+            Profile
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            value="notification"
+            className="text-white data-[state=on]:text-primary-400 data-[state=on]:bg-white font-Inconsolata gap-4 w-full justify-start"
+          >
+            <Notification />
+            Notification
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            value="settings"
+            className="text-white data-[state=on]:text-primary-400 data-[state=on]:bg-white font-Inconsolata gap-4 w-full justify-start"
+          >
+            <Setting />
+            Settings
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            value="help"
+            className="text-white data-[state=on]:text-primary-400 data-[state=on]:bg-white font-Inconsolata gap-4 w-full justify-start"
+          >
+            <Message />
+            Help & Support
+          </ToggleGroupItem>
+        </ToggleGroup>
+      </div>
+      {/* content */}
+      <div className="flex flex-col justify-center p-6 grow pt-12">
+        <h1 className="self-start font-semibold text-3xl">
+          Welcome to SchoolSync
+        </h1>
+        <div className="h-full w-full flex justify-center items-center flex-col">
+          <div className="flex flex-col items-center">
+            <Image
+              src={fileImg}
+              alt="no files"
+              className="h-[350px] w-[350px]"
+            />
+            <span>No records yet</span>
+          </div>
+          <span className="mt-8">
+            Go to Profile to fill in your required information
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/public/icons/activity.tsx
@@ -0,0 +1,30 @@
+export const Activity = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="vuesax/linear/activity">
+      <g id="activity">
+        <path
+          id="Vector"
+          d="M7.50033 18.3327H12.5003C16.667 18.3327 18.3337 16.666 18.3337 12.4993V7.49935C18.3337 3.33268 16.667 1.66602 12.5003 1.66602H7.50033C3.33366 1.66602 1.66699 3.33268 1.66699 7.49935V12.4993C1.66699 16.666 3.33366 18.3327 7.50033 18.3327Z"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_2"
+          d="M6.1084 12.0758L8.09173 9.50078C8.37507 9.13411 8.90007 9.06745 9.26673 9.35078L10.7917 10.5508C11.1584 10.8341 11.6834 10.7674 11.9667 10.4091L13.8917 7.92578"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </g>
+  </svg>
+);
